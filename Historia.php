79a2573b2153c72031ffc2183734aae9c8a19e51
jose-khea <70334102+jose-khea@users.noreply.github.com>
2020-09-02 20:58:50 -0500

    Add files via upload

---
+++ Historia.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title ></title>
+	<link rel="stylesheet" type="text/css" href="lean.css">
+</head>
+<body background="p.jpg">
+
+
+   <center>
+	<div class="Borde">
+
+		 <font face="Cooper Black"><h1 style="color: white">MASACRES EN COLOMBIA 🌺✨🤍</h1></font>
+
+		 <h3 style="color: white"> "los buenos somos mas", dedicado a las buenas personas que trataban de buscar un
+		 lugar mejor para ellos, pero lamentablemente l¿no le importan al gobierno, ¿Cuantas muertes nescesitamos?
+		para valorar la vida.......</h3>
+
+	</div>
+
+	<div class="img">
+
+	 <img src="po.png" height="1080" width="1080" >
+
+	 </div>
+
+
+	 <div class="Samaniego">  <font face="Jokerman">Samaniego 💀</font></div>
+	 
+	 <div class="s1"> <h4 style="color: white"> en Nariño, se disputan el territoria las bandas criminales
+	 y los grupos paramilitares, por lo que es indignate ver como acesinaron a 8 jovenes, que es que los
+	 confundieron con integrantes de las FARC, pero es que en Samaniego no se dan oportunidades, no hay
+	estudios superiores, ni hospitales aptos por lo que el estado no esta presente aqyui y se pasa el  
+	domino al paramilitarismo, y las bandas quienes creen que la vida es un algo que se manipula, los 
+	jovenes eran personas de bien que solo querian un municipio mejor y tranquilo 🌻</h4>
+
+	<img src="joven.jpg" height="150" width="200"> 
+
+
+   </div>
+
+     <div class="Cali">  <font face="Jokerman">Cali 💀</font></div>
+	 
+	 <div class="c1"> <h4 style="color: white">En Cali se presenta una gran vrecha de desigualdad,
+	 pot lo que en las zonas "populares" de la cuidad se haga muy facil reclutar niños para los 
+	grupos paramilitares y lo que hizo la alcaldia fue bajarle la atencion y presupuesto a los 
+    programas de integracion de los jovenes, y una cosa muy impactante que dijo una madre "por
+    el hecho de ser pobres y negros nos pasa estos " aqui es donde pasan los problemas la policia
+     de Cali prefiere cerrar moteles que proteger la vida de los jovenes, porque para eso si no
+     son policias aqui le trae mas sufrimiento a familias que de por si ya estan jodidas🌹
+    <img src="f.jpg" height="150" width="200"> 
+
+    </h4></div>
+
+     <div class="Suro">  <font face="Jokerman">Venecia y Andes 💀</font></div>
+	 
+	 <div class="su1"> <h4 style="color: white">En el Suroeste antioueño, aunque es un lugar tranquilo
+	 se comenzaron a denotar dos masacres en jovenes las cuales evidencian como cada vez mas la violencia
+	se esta volviendo a exparsir y dominar rincones se evidencian en las masacres de Venecia en la cual
+	estaban involucrados jovenes con drogas, para mi si se enfocaran en la tencion y generar oportunidades
+en las areas rurales del municipio, por lo que la falta de atencion y el olvido a la juventud rural es 
+algo que conlleva estas situaciones</h4>
+
+ <img src="VEN.jpg" height="150" width="200"> 
+
+</div>
+
+
+     <div class="Catatumbo">  <font face="Jokerman">Catatumbo 💀</font></div>
+	 
+	 <div class="ct1"> <h4 style="color: white">Cuando se habla del Catatumbo estamos ahblando de una guerra
+	 sin fin puesto que aqui a pesar de ser una zona de reingression de guerrilleros, estos son quienes mas han 
+	tenido que sufrir por lo que viven entre la pared y la espada porque el gobierno no hace nada ni se encarga de 
+ ver los acuerdos y el ELN y EPL se encargan de atentar contra la vida de estas personas aqui se reportaron 3 muertes
+    de ex-guerrilleros</h4>
+
+    <img src="cy.jpg" height="150" width="200"> 
+
+     </div>
+
+
+     <div class="Cauca">  <font face="Jokerman">Cauca 💀</font></div>
+	 
+	 <div class="ca1"> <h4 style="color: white">El Cauca es unos de los departamentos mas letales para ser lideres social 
+	 porque aqui se concentran bandas criminales, cultivos de Coca, Proyectos Mega extractivistas para la explotacion de 
+	 los recursos y el estado no se reune ni con los lideres sociales ni con la MINGA indigena para arreglar por lo que
+	  en el territoria se ha vuelto deporte matar a los lideres ocacionando muertes de campesinos, indigenas y civiles
+	  que lo unico que exigen es un lugar mejor y tranqulo pero el estado se hizo el ciego con esto </h4>
+
+      <img src="MS.jpg" height="150" width="200"> 
+
+	</div>
+
+
+
+
+
+	</center>
+
+</body>
+</html>
